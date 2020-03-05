43cca720bf34951c487f7739e4592274441a91ec
qiuzhanghua <qiuzhanghua@icloud.com>
2020-03-05 18:37:25 +0800

    add mysql and pg for test

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/qiuzhanghua/redis_go
 go 1.14
 
 require github.com/go-redis/redis/v7 v7.2.0
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+//	github.com/jinzhu/gorm v1.9.12
+ 	github.com/lib/pq v1.3.0
+)
--- hello.go
+++ hello.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 import "github.com/go-redis/redis/v7"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+import _ "github.com/lib/pq"
 
 func main() {
 	client := redis.NewClient(&redis.Options{
@@ -9,8 +12,20 @@ func main() {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+	defer client.Close()
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 	//var r = client.HGetAll("company:1")
 	//fmt.Println(r)
+	db1, err := sql.Open("mysql", "app:app@/app")
+	defer db1.Close()
+	err = db1.Ping()
+	fmt.Println(err)
+
+	db2, err := sql.Open("postgres",
+		"host=localhost port=5432 user=app password=app dbname=app sslmode=disable")
+	defer db2.Close()
+	err = db2.Ping()
+	fmt.Println(err)
+
 }
