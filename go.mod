317443ab23e70b982e75b040f5f07f5dbdcb9aa9
qiuzhanghua <qiuzhanghua@icloud.com>
2020-04-15 12:41:50 +0800

    Add Iris

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/go-redis/redis/v7 v7.2.0
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
-//	github.com/jinzhu/gorm v1.9.12
- 	github.com/lib/pq v1.3.0
+	github.com/kataras/iris/v12 v12.1.8
+	//	github.com/jinzhu/gorm v1.9.12
+	github.com/lib/pq v1.3.0
+	github.com/shurcooL/sanitized_anchor_name v1.0.0
 )
--- hello.go
+++ hello.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/logger"
+	"github.com/kataras/iris/v12/middleware/recover"
+)
 import "github.com/go-redis/redis/v7"
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
@@ -34,4 +39,33 @@ func main() {
 	err = db2.Ping()
 	fmt.Println(err)
 
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	// Method:   GET
+	// Resource: http://localhost:8080
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.HTML("<h1>Welcome</h1>")
+	})
+
+	// same as app.Handle("GET", "/ping", [...])
+	// Method:   GET
+	// Resource: http://localhost:8080/ping
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.WriteString("pong")
+	})
+
+	// Method:   GET
+	// Resource: http://localhost:8080/hello
+	app.Get("/hello", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "Hello Iris!"})
+	})
+
+	// http://localhost:8080
+	// http://localhost:8080/ping
+	// http://localhost:8080/hello
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
