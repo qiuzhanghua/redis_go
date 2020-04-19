c0cd74f8d4c2d4bb2a36533dd4e30fcf72eba3ff
Daniel <qiuzhanghua@icloud.com>
2020-04-19 18:48:33 +0800

    try redis

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Redis for Golang
 
 learn
+
+##  Perf
+```text
+➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080
+Running 30s test @ http://127.0.0.1:8080
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     4.57ms    2.17ms  79.47ms   98.92%
+    Req/Sec     3.45k   303.20     5.91k    82.87%
+  1237660 requests in 30.10s, 153.44MB read
+  Socket errors: connect 0, read 37, write 0, timeout 0
+Requests/sec:  41112.81
+Transfer/sec:      5.10MB
+```
--- hello.go
+++ hello.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/middleware/logger"
 	"github.com/kataras/iris/v12/middleware/recover"
 )
 import "github.com/go-redis/redis/v7"
-import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
 import _ "github.com/lib/pq"
 
@@ -18,37 +15,42 @@ func main() {
 		DB:       0,  // use default DB
 	})
 	defer client.Close()
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	//var r = client.HGetAll("company:1")
-	//fmt.Println(r)
-	db1, err := sql.Open("mysql", "app:app@/app")
-	if err != nil {
-		return
-	}
-	defer db1.Close()
-	err = db1.Ping()
-	fmt.Println(err)
-
-	db2, err := sql.Open("postgres",
-		"host=localhost port=5432 user=app password=app dbname=app sslmode=disable")
-	if err != nil {
-		return
-	}
-	defer db2.Close()
-	err = db2.Ping()
-	fmt.Println(err)
+	//pong, err := client.Ping().Result()
+	//fmt.Println(pong, err)
+	////var r = client.HGetAll("company:1")
+	////fmt.Println(r)
+	//db1, err := sql.Open("mysql", "app:app@/app")
+	//if err != nil {
+	//	return
+	//}
+	//defer db1.Close()
+	//err = db1.Ping()
+	//fmt.Println(err)
+	//
+	//db2, err := sql.Open("postgres",
+	//	"host=localhost port=5432 user=app password=app dbname=app sslmode=disable")
+	//if err != nil {
+	//	return
+	//}
+	//defer db2.Close()
+	//err = db2.Ping()
+	//fmt.Println(err)
 
 	app := iris.New()
-	app.Logger().SetLevel("debug")
+	//app.Logger().SetLevel("debug")
 
 	app.Use(recover.New())
-	app.Use(logger.New())
+	//	app.Use(logger.New())
 
 	// Method:   GET
 	// Resource: http://localhost:8080
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Welcome</h1>")
+		cmd := client.Get("key")
+		if cmd.Err() != redis.Nil {
+			ctx.WriteString(cmd.Val())
+		} else {
+			ctx.StatusCode(iris.StatusNotFound)
+		}
 	})
 
 	// same as app.Handle("GET", "/ping", [...])
